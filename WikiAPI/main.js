d88cbb369ed8e3b9145d98e1824b9bb203f04e67
Ian <imatthews009@gmail.com>
2017-11-05 21:39:26 -0800

    added wiki API project

---
+++ WikiAPI/main.js
@@ -0,0 +1,25 @@
+function searchWiki(searchedWords) {
+  var apiFriendlySearchedWords = searchedWords.replace(" ", "%20");
+
+  $.ajax({
+    url: "https://en.wikipedia.org/w/api.php?",
+    dataType: 'jsonp',
+    data: {
+      action: 'opensearch',
+      format: 'json',
+      search: apiFriendlySearchedWords
+    },
+    success: function(results) {
+      console.log(results);
+      console.log(results[1]);
+      for(var i = 0; i < results[1].length; i++) {
+        $('.insertedWikis').prepend("<div class='wikiCards' id='card" + (i+1) + "'><h1><a href='" + results[3][i] + "'>" + results[1][i] + "</a></h1><h2>" + results[2][i] + "</h2></div>");
+        console.log(results[3][i]);
+      }
+    }
+  })
+}
+
+function randomWiki() {
+  location.href = "https://en.wikipedia.org/wiki/Special:Random"
+}
